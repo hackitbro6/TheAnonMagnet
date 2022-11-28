0be1325ff758214e80c5f7451edd1e283f2aa90f
kindevilsorg <117510982+kindevilsorg@users.noreply.github.com>
2022-11-28 21:33:13 +0530

    Update torrent.py

--- torrent.py
+++ torrent.py
@@ -1,5 +1,5 @@
 env_file = os.getenv('GITHUB_ENV')
 from torrentp import TorrentDownloader
-x = ${{ github.event.inputs.link }}
+x =  github.event.inputs.link 
 torrent_file = TorrentDownloader(x , '.')
 torrent_file.start_download()
