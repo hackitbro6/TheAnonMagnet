4131b4a4305b058571851747e28dffc1903e6a2e
kindevilsorg <117510982+kindevilsorg@users.noreply.github.com>
2022-11-28 21:36:03 +0530

    Update torrent.py

--- torrent.py
+++ torrent.py
@@ -1,5 +1,5 @@
 
 from torrentp import TorrentDownloader
-x =  github.event.inputs.link 
+x =  ${{ github.event.inputs.Token }}
 torrent_file = TorrentDownloader(x , '.')
 torrent_file.start_download()
