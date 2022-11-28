6260c02f710469c8708b3d3436877ea03032d75f
kindevilsorg <117510982+kindevilsorg@users.noreply.github.com>
2022-11-28 21:52:14 +0530

    Update torrent.py

--- torrent.py
+++ torrent.py
@@ -3,6 +3,6 @@ import os
 input_variable = os.environ['MAIN_LINK']
 
 from torrentp import TorrentDownloader
-x =  (MAIN_LINK)
+x =  (input_variable)
 torrent_file = TorrentDownloader(x , '.')
 torrent_file.start_download()
